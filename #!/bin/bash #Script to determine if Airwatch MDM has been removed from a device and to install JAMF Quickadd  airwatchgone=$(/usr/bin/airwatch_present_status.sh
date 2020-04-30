d1dcfd67732d64cb04e85b6ae4d5956e522e5361
alexmcclements <64583594+alexmcclements@users.noreply.github.com>
2020-04-30 11:21:56 +0200

    Create airwatch_present_status.sh
    You can use this script to determine if a device is still enrolled with AirWatch. The assumption is that once have deleted the device from AirWatch all the Configuration Profiles have been removed from the device.

---
+++ #!/bin/bash #Script to determine if Airwatch MDM has been removed from a device and to install JAMF Quickadd  airwatchgone=$(/usr/bin/airwatch_present_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Script to determine if Airwatch MDM has been removed from a device and to install JAMF Quickadd
+
+airwatchgone=$(/usr/bin/profiles -P)
+
+if [ "$airwatchgone" == "There are no configuration profiles installed" ]; then echo "Airwatch Uninstalled"
+
+else echo "AirWAatch still installed"
+
+fi
